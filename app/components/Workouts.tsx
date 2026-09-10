678fff7b72457ff6bba95ece844dead31044556d
Jake Garcia <bballwiz4@gmail.com>
2026-09-10 16:21:29 -0700

    Addede styling to workout card.

--- app/components/Workouts.tsx
+++ app/components/Workouts.tsx
@@ -16,7 +16,7 @@ export default async function Workouts() {
   return (
     <div className="flex flex-col w-full">
       <h1 className="text-3xl font-bold font-sans">Workouts</h1>
-      <div className="flex flex-row px-2 py-2 bg-gray-800 rounded-lg h-64">
+      <div className="flex flex-row px-2 py-2 bg-gray-700/40 rounded-lg h-64 shadow-inner shadow-gray-600/50">
         {renderWorkouts()}
       </div>
     </div>
