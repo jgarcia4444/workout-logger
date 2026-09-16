0f67fa30347441f669c8f911880ce918f935f9a1
Jake Garcia <bballwiz4@gmail.com>
2026-09-15 19:54:40 -0700

    Moved the finding workout logic up to the workout dashboard parent component.

--- app/components/WorkoutDashboard/WorkoutInfo.tsx
+++ app/components/WorkoutDashboard/WorkoutInfo.tsx
@@ -1,20 +1,6 @@
-"use client"
-import { useEffect, useState } from "react";
-import { findWorkout } from "@/app/actions/findWorkout"
 
-export default function WorkoutInfo({workoutId}: {workoutId: string}) {
-
-    const [workout, setWorkout] = useState<{name: string, id: number} | null>(null);
-
-    useEffect(() => {
-        const fetchWorkout = async () => {
-            const workout = await findWorkout(workoutId);
-            setWorkout(workout);
-        }
-        fetchWorkout();
-    }, [workoutId])
-
-       
+export default function WorkoutInfo({workout}: {workout: {name: string, id: number} | null}) {
+    
     return (
         <div className="w-full flex items-start justify-center">
             <div className="w-full flex flex-row items-start px-2 py-4 bg-gray-700/40 rounded-lg shadow-inner shadow-gray-600/50 relative">
--- app/workouts/workout-dashboard/page.tsx
+++ app/workouts/workout-dashboard/page.tsx
@@ -1,12 +1,25 @@
 'use client';
 
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
+import { findWorkout } from "@/app/actions/findWorkout";
+
 import WorkoutInfo from "@/app/components/WorkoutDashboard/WorkoutInfo";
 
 export default function WorkoutDashboard() {
 
     const searchParams = useSearchParams();
     const workoutId = searchParams.get("workoutId");
+    const [workout, setWorkout] = useState<{name: string, id: number} | null>(null);
+    
+    useEffect(() => {
+        if (workoutId) {
+            findWorkout(workoutId).then((workout) => {
+                setWorkout(workout);
+            });
+        }
+    }, [workoutId])
+
     
 
     if (!workoutId) {
@@ -19,7 +32,7 @@ export default function WorkoutDashboard() {
 
     return (
         <div className="w-full mx-auto p-2 flex flex-col gap-2">
-            <WorkoutInfo workoutId={workoutId} />
+            <WorkoutInfo workout={workout} />
             <div className="flex flex-row gap-2 w-full">
                 <div className="w-2/3 px-2 py-4 bg-gray-700/40 rounded-lg shadow-inner shadow-gray-600/50">
                     wec
