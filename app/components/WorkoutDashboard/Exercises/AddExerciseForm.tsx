99be49ac39c9ea7d85925bac620cad1a0f32ef47
Jake Garcia <bballwiz4@gmail.com>
2026-09-18 23:21:12 -0700

    Added in labels to help identify which inputs are to hold which value.

--- app/components/WorkoutDashboard/Exercises/AddExerciseForm.tsx
+++ app/components/WorkoutDashboard/Exercises/AddExerciseForm.tsx
@@ -1,17 +1,65 @@
+'use client';
 
+import {useState} from 'react';
 export default function AddExerciseForm() {
+
+    const [exerciseName, setExerciseName] = useState('');
+    const [exerciseDescription, setExerciseDescription] = useState('');
+    const [sets, setSets] = useState(0);
+    const [reps, setReps] = useState(0);
+    const [weight, setWeight] = useState(0);
+
+    const handleAddExercise = () => {
+        // TODO: Add exercise logic
+    }
+
+    const handleExerciseNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setExerciseName(e.target.value);
+    }
+
+    const handleExerciseDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setExerciseDescription(e.target.value);
+    }
+
+    const handleSetsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSets(parseInt(e.target.value));
+    }
+
+    const handleRepsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setReps(parseInt(e.target.value));
+    }
+
+    const handleWeightChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setWeight(parseInt(e.target.value));
+    }
+
     return (
         <div className="w-full flex flex-col items-center justify-center">
             <form className="w-full flex flex-col gap-2 h-full">
-                <input className="w-full bg-black/40 rounded-lg p-2 shadow-inner shadow-black text-white" type="text" placeholder="Exercise Name" />
-                <textarea className="w-full bg-black/40 rounded-lg p-2 shadow-inner shadow-black text-white" placeholder="Exercise Description" />
+                <div className="w-full flex flex-col">
+                    <label className="text-black font-bold text-sm">Exercise Name</label>
+                    <input className="w-full bg-black/40 rounded-lg p-2 shadow-inner shadow-black text-white" type="text" placeholder="Exercise Name" value={exerciseName} onChange={handleExerciseNameChange} />
+                </div>
+                <div className="w-full flex flex-col">
+                    <label className="text-black font-bold text-sm">Exercise Description</label>
+                    <textarea className="w-full bg-black/40 rounded-lg p-2 shadow-inner shadow-black text-white" placeholder="Exercise Description" value={exerciseDescription} onChange={handleExerciseDescriptionChange} />
+                </div>
                 <div className="w-full flex gap-2">
-                    <input className="w-full bg-black/40 rounded-lg p-2 shadow-inner shadow-black text-white" type="number" placeholder="Sets" />
-                    <input className="w-full bg-black/40 rounded-lg p-2 shadow-inner shadow-black text-white" type="number" placeholder="Reps" />
-                    <input className="w-full bg-black/40 rounded-lg p-2 shadow-inner shadow-black text-white" type="number" placeholder="Weight" />
+                    <div className="w-full flex flex-col">
+                        <label className="text-black font-bold text-sm">Sets</label>
+                        <input className="w-full bg-black/40 rounded-lg p-2 shadow-inner shadow-black text-white" type="number" placeholder="Sets" value={sets} onChange={handleSetsChange} />
+                    </div>
+                    <div className="w-full flex flex-col">
+                        <label className="text-black font-bold text-sm">Reps</label>
+                        <input className="w-full bg-black/40 rounded-lg p-2 shadow-inner shadow-black text-white" type="number" placeholder="Reps" value={reps} onChange={handleRepsChange} />
+                    </div>
+                    <div className="w-full flex flex-col">
+                        <label className="text-black font-bold text-sm">Weight</label>
+                        <input className="w-full bg-black/40 rounded-lg p-2 shadow-inner shadow-black text-white" type="number" placeholder="Weight" value={weight} onChange={handleWeightChange} />
+                    </div>
                 </div>
                 <div className="w-full flex items-end justify-end">
-                    <button className="px-4 py-2 bg-gray-700/40 rounded-lg hover:cursor-pointer w-48 text-black font-bold transition-all duration-200 hover:bg-gray-500/60 hover:text-white active:bg-gray-700/80 active:text-white" type="submit">Add Exercise</button>
+                    <button onClick={handleAddExercise} className="px-4 py-2 bg-gray-700/40 rounded-lg hover:cursor-pointer w-48 text-black font-bold transition-all duration-200 hover:bg-gray-500/60 hover:text-white active:bg-gray-700/80 active:text-white" type="submit">Add Exercise</button>
                 </div>
             </form>
         </div>
